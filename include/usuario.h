60d3f357aa8dd23ce18e020a318f8246b592933f
matheusriv <matheus.r.slv@gmail.com>
2021-07-30 16:07:34 -0300

    edit comentarios usuarios.h

--- include/usuario.h
+++ include/usuario.h
@@ -5,10 +5,10 @@
 
 class Usuario{
   private:
-    int id;            //<! ID do usuário.
-    std::string nome;  //<! Nome do usuário.
-    std::string email; //<! Endereço de email do usuário.
-    std::string senha; //<! Senha do login.
+    int id;            //<! ID do usuário
+    std::string nome;  //<! Nome do usuário
+    std::string email; //<! Endereço de email do usuário
+    std::string senha; //<! Senha do login
 
   public:
     /* Construtor */
@@ -18,50 +18,50 @@ class Usuario{
     Usuario();
 
     /*
-     * @brief Altera o ID do usuário.
-     * @param id ID novo.
+     * @brief Altera o ID do usuário
+     * @param id ID novo
      */
     void setId(int id);
 
     /*
-     * @brief Altera o nome do usuário.
-     * @param nome Novo nome.
+     * @brief Altera o nome do usuário
+     * @param nome Novo nome
      */
     void setNome(const std::string nome);
 
     /*
-     * @brief Altera o email do usuario.
-     * @param email Novo email.
+     * @brief Altera o email do usuario
+     * @param email Novo email
      */
     void setEmail(const std::string email);
 
     /*
-     * @brief Altera a senha do usuário.
-     * @param senha Nova senha.
+     * @brief Altera a senha do usuário
+     * @param senha Nova senha
      */
     void setSenha(const std::string senha);
 
     /*
-     * @brief Retorna o ID do usuário.
-     * @return Inteiro do ID do usuário.
+     * @brief Retorna o ID do usuário
+     * @return Inteiro do ID do usuário
      */
     int getId();
 
     /*
-     * @brief Retorna o Nome do usuário.
-     * @return String com o nome do usuário.
+     * @brief Retorna o Nome do usuário
+     * @return String com o nome do usuário
      */
     std::string getNome();
 
     /*
-     * @brief Retorna o Email do usuário.
-     * @return String com o email do usuário.
+     * @brief Retorna o Email do usuário
+     * @return String com o email do usuário
      */
     std::string getEmail();
 
     /*
-     * @brief Retorna a senha do usuário.
-     * @return String com a senha do usuário.
+     * @brief Retorna a senha do usuário
+     * @return String com a senha do usuário
      */
     std::string getSenha();
 
